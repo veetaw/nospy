8436c8dbafcc464c50495bdf253f4e9ccf28d55a
veetaw <veetaw@users.noreply.github.com>
2017-12-04 16:06:09 +0100

    added Audio and removed unit tests
    Signed-off-by: veetaw <veetaw@users.noreply.github.com>

---
+++ app/src/main/java/com/veetaw/nospy/util/Audio.kt
@@ -0,0 +1,36 @@
+package com.veetaw.nospy.util
+
+import java.io.File
+
+class Audio {
+
+    private fun getCard(): CharArray {
+        val card = charArrayOf('0', '0')
+
+        val path = "/proc/asound/devices"
+        val file = File(path).inputStream().bufferedReader().use { it.readText() }
+        val pattern = Regex("\\d+: \\[ (\\d)- (\\d)]: digital audio capture")
+
+        try {
+            card[0] = pattern.matchEntire(pattern.findAll(file).toList()[0].value)?.groups?.get(1)?.value?.single() as Char
+            card[1] = pattern.matchEntire(pattern.findAll(file).toList()[0].value)?.groups?.get(2)?.value?.single() as Char
+        } catch (e: Exception) {
+        } // do nothing, ['0', '0'] will be used
+
+        return card
+    }
+
+    fun isUsed(): Boolean {
+        val card = this.getCard()
+        val path = "/proc/asound/card" + card[0] + "/pcm" + card[1] + "c/info"
+        val file = File(path).inputStream().bufferedReader().use { it.readText() }
+
+        var pattern = Regex("subdevices_count: (\\d)")
+        val total = Integer.parseInt(pattern.matchEntire(pattern.findAll(file).toList()[0].value)?.groups?.get(1)?.value)
+
+        pattern = Regex("subdevices_avail: (\\d)")
+        val available = Integer.parseInt(pattern.matchEntire(pattern.findAll(file).toList()[0].value)?.groups?.get(1)?.value)
+
+        return total - available > 0
+    }
+}
